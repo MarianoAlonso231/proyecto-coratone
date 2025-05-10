486bf94a29644f8aa9abfa4e92a71bcf8145efed
MarianoAlonso231 <168044214+MarianoAlonso231@users.noreply.github.com>
2025-05-10 20:25:52 -0300

    Set up admin panel with Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import ProductSection from './components/ProductSection';
 import Footer from './components/Footer';
-import WhatsAppButton from './components/WhatsAppButton';
-import AdminLogin from './components/admin/AdminLogin';
-import AdminDashboard from './components/admin/AdminDashboard';
-import ProtectedRoute from './components/admin/ProtectedRoute';
 import { getProductsByCategory } from './data/products';
+import WhatsAppButton from './components/WhatsAppButton';
 
 function App() {
   const rings = getProductsByCategory('ring');
@@ -16,43 +11,18 @@ function App() {
   const bracelets = getProductsByCategory('bracelet');
 
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Routes>
-          {/* Admin routes */}
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route
-            path="/admin/*"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* Public routes */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <HeroSection />
-                <ProductSection id="anillos" title="Anillos" products={rings} />
-                <ProductSection id="collares" title="Collares" products={necklaces} />
-                <ProductSection id="aritos" title="Aritos" products={bracelets} />
-                <Footer />
-                <WhatsAppButton />
-              </>
-            }
-          />
-          
-          {/* Catch all route */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <Toaster position="top-right" />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <HeroSection />
+      <ProductSection id="anillos" title="Anillos" products={rings} />
+      <ProductSection id="collares" title="Collares" products={necklaces} />
+
+      <ProductSection id="aritos" title="Aritos" products={bracelets} />
+
+      <Footer />
+      <WhatsAppButton />
+    </div>
   );
 }
 
-export default App;
+export default App
